3a0d4f15714363a7680cc08a6cd89f2a0412d873
Wei-Tse Hsu <wehs7661@colorado.edu>
2022-02-06 22:42:22 -0700

    Improved transition identification in pairwise_rmsd.py

--- analysis_results/supporting_information/pairwise_RMSD/pairwise_rmsd.py
+++ analysis_results/supporting_information/pairwise_RMSD/pairwise_rmsd.py
@@ -19,25 +19,25 @@ def initialize():
 
     return args_parse
 
-def locate_transition(xpm_data, r=10):
+def locate_transition(xpm_data, r=500, N=10):
     """
     This function locates the rough time frame where the biggest transition occurs.
     """
-    rmsd_diff = []
-    idx = range(r, len(xpm_data) - r)
-    for i in idx:
-        # In the case that range=10, for time frame i, we average the RMSD of the corresponding structure w.r.t. the 
-        # structure at i-1, i-2, ..., i-10 (rsmd_1) and at i + 1, i + 2, ..., i + 10 (rmsd_2)
-        # The time frame having the biggest difference between rmsd_1 and rmsd_2 is what we look for. 
-        rmsd_1 = np.mean(xpm_data[i - r : i - 1, i])
-        rmsd_2 = np.mean(xpm_data[i + 1 : i + r, i])
-        rmsd_diff.append(np.abs(rmsd_1 - rmsd_2))
+    rmsd_1d = xpm_data[0]   # The rmsd value of the initial structure w.r.t all others
+    diff = []
+    for i in range(r, len(rmsd_1d) - r):
+        avg_rmsd = [np.mean(rmsd_1d[i - r : i]), np.mean(rmsd_1d[i: i + r])]
+        diff.append(max(avg_rmsd) / min(avg_rmsd)) 
 
-    transition_idx = idx[rmsd_diff.index(np.max(rmsd_diff))]
-    print(rmsd_diff[transition_idx])
-    transition = transition_idx * (2000 / (len(xpm_data) - 1))   # ns
-    
+    # Below we find the time frames corresopnding to the largest differences
+    diff_idx = list(np.argsort(diff))  # idx of increasing values
+    diff_idx.reverse()   # idx of decreasing values
+    idx = np.array(diff_idx[:N])   # The indices of the N largest numbers
+
+    transition = idx * (2000 / (len(xpm_data) - 1))  # ns
+
     return transition
+
 
 
 if __name__ == "__main__":
